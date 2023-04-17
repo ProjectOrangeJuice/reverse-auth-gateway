8e562019c35ca68eac6e47be7a099dba0e3e37f4
ProjectOrange <theJuggernaut@harriso.co.uk>
2023-04-17 21:09:11 +0100

    add limiter

--- gateway.go
+++ gateway.go
@@ -4,15 +4,22 @@ import (
 	"gateway/web"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/didip/tollbooth"
+	"github.com/didip/tollbooth/limiter"
+	"github.com/didip/tollbooth_gin"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	handlers := web.SetupHandlers()
 	router := gin.Default()
+	// Create a new limiter that allows 5 requests per second with a burst limit of 5.
+	lim := tollbooth.NewLimiter(5, &limiter.ExpirableOptions{DefaultExpirationTTL: time.Hour})
+	lim.SetBurst(5)
 
-	router.POST("/unlock", handlers.UnlockPage)
+	router.POST("/unlock", tollbooth_gin.LimitHandler(lim), handlers.UnlockPage)
 	router.GET("/unlock", handlers.UnlockPage)
 	router.GET("/access", handlers.AccessPage)
 	router.GET("/audit", handlers.AuditPage)
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module gateway
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.9.0
+require (
+	github.com/didip/tollbooth v4.0.2+incompatible
+	github.com/didip/tollbooth_gin v0.0.0-20170928041415-5752492be505
+	github.com/gin-gonic/gin v1.9.0
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	golang.org/x/time v0.3.0 // indirect
+)
