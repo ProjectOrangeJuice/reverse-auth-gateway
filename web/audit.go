5cb7c59128c795cbfe80db6b731224e09cce819b
ProjectOrange <theJuggernaut@harriso.co.uk>
2023-04-17 21:51:25 +0100

    Add bucket to view

--- web/audit.go
+++ web/audit.go
@@ -3,5 +3,9 @@ package web
 import "github.com/gin-gonic/gin"
 
 func (h *Handlers) AuditPage(g *gin.Context) {
-	h.Templates.ExecuteTemplate(g.Writer, "audit", allowed)
+	h.auditLock.Lock()
+	copiedGranted := make([]*authed, len(h.granted))
+	copy(copiedGranted, h.granted)
+	h.auditLock.Unlock()
+	h.Templates.ExecuteTemplate(g.Writer, "audit", copiedGranted)
 }
