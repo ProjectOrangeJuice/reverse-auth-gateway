cfc7bec76b74502f78b1bf05e68ea7486df6d65e
ProjectOrangeJuice <YOUR_NOREPLY_EMAIL>
2026-04-17 20:39:41 +0100

    setup publishing docker image

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
  CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gateway
- docker build -t gateway .
- docker save gateway > gatewayDocker
+ docker build -t projectorangejuice/gateway .
+ docker push projectorangejuice/gateway
